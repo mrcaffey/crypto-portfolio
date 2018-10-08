14cf10a0a234bc7643962bed1d42fff858c334f6
Micah Caffey <mrcaffey@gmail.com>
2018-10-08 12:35:28 -0600

    created/updated coin.js

--- client/src/components/CoinList.js
+++ client/src/components/CoinList.js
@@ -9,8 +9,14 @@ import { getCoins } from '../reducers/coins'
 
 class CoinList extends React.Component {
   componentDidMount() {
-    this.props.dispatch(getCoins())
+    const { dispatch } = this.props
+    dispatch(getCoins())
+    this.interval = setInterval( () => dispatch(getCoins()), 60000 )
   }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+    }
 
   headers = () => {
     return ['Symbol', 'Name', 'Price'].map( header =>
