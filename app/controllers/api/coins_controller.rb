aeb78e0fa5eead459dd09861a74402eb1d3c8ed1
Micah Caffey <mrcaffey@gmail.com>
2018-10-08 21:40:16 -0600

    updated coins controller

--- app/controllers/api/coins_controller.rb
+++ app/controllers/api/coins_controller.rb
@@ -2,24 +2,20 @@ class Api::CoinsController < ApplicationController
   before_action :set_coin, only: [:update, :destroy]
   BASE_URL = 'https://api.coinmarketcap.com/v2/'
 
-
   def index
     listings = HTTParty.get("#{BASE_URL}ticker")['data'].values
     user_coins = current_user.coins
     user_coins.each do |coin|
-      res_coin = listings.find { |c| c['id'] === coin.cmc_id}
-      coin.update(price: res_coin['quotes']['USD]]['price'])
-  end
-
-
+      res_coin = listings.find { |c| c['id'] === coin.cmc_id }
+      coin.update(price: res_coin['quotes']['USD']['price'])
+    end
 
     render json: current_user.coins
   end
 
-
   # POST /api/coins ?coin=btc
   def create
-    cmc_id= params[:coin].upcase
+    cmc_id = params[:coin].upcase  
     listings = HTTParty.get("#{BASE_URL}listings")
     listing = listings['data'].find { |l| l['symbol'] == cmc_id }
     res = HTTParty.get("#{BASE_URL}/ticker/#{listing['id']}")
@@ -47,8 +43,10 @@ class Api::CoinsController < ApplicationController
     @coin.destroy
   end
 
-
   private
     def set_coin
-    @coin = Coin.find(params[:id])
+      @coin = Coin.find(params[:id])
+    end
+
+
 end
